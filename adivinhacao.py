b7448aa7704da2728eb1152d115cac92407d11dc
violiveiradev <violiveira.dev@gmail.com>
2022-01-17 00:34:47 -0300

    Conclusão da parte 1 do curso python3.

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,16 +1,54 @@
-print("*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************")
 
-numero_secreto = 42
+def jogar():
+    import random
 
-chute = int(input("Digite um numero: "))
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
 
-print("Você digitou: ", chute)
+    numero_secreto = random.randrange(1, 101)
+    total_de_tentativas = 0
+    pontos = 1000
 
-if (numero_secreto == chute):
-    print("Você acertou.")
-else:
-    print("Você não acertou.")
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
 
-print("Fim do jogo!")
+    nivel = int(input("Define o nível: "))
+
+    if (nivel == 1):
+        total_de_tentativas = 20
+    elif (nivel == 2):
+        total_de_tentativas = 10
+    else:
+        total_de_tentativas = 5
+
+    for rodada in range(1, total_de_tentativas + 1):
+        print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
+
+        chute = int(input("Digite um número entre 1 e 100: "))
+        print("Você digitou: ", chute)
+
+        if(chute < 1 or chute > 100):
+            print("Você deve digitar um número entre 1 e 100: ")
+            continue
+
+        acertou = chute == numero_secreto
+        maior   = chute > numero_secreto
+        menor   = chute < numero_secreto
+
+        if (acertou):
+            print("Você acertou e fez {} pontos.".format(pontos))
+            break
+        else:
+           if(maior):
+               print("Você não acertou, digitou o numero maior que o numero secreto.")
+           elif(menor):
+               print("Você não acertou, digitou o numero menor que o numero secreto.")
+           pontos_perdidos = abs(numero_secreto - chute)
+           pontos -= pontos_perdidos
+
+    print("Fim do jogo!")
+
+
+if (__name__ == "__main__"):
+    jogar()
